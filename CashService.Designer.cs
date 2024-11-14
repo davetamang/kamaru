881b53ac4f947db2560b51e116a18df948541119
kamaru-stack <kamarukarki455@gmail.com>
2024-11-14 22:36:51 +0545

    Add files via upload

---
+++ CashService.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Car_Wash_Management
+{
+    partial class CashService
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CashService));
+            this.dgvServices = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnCash = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServices)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvServices
+            // 
+            this.dgvServices.AllowUserToAddRows = false;
+            this.dgvServices.AllowUserToDeleteRows = false;
+            this.dgvServices.AllowUserToResizeColumns = false;
+            this.dgvServices.AllowUserToResizeRows = false;
+            this.dgvServices.BackgroundColor = System.Drawing.Color.White;
+            this.dgvServices.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(85)))), ((int)(((byte)(245)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvServices.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvServices.ColumnHeadersHeight = 29;
+            this.dgvServices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvServices.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Select});
+            this.dgvServices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvServices.EnableHeadersVisualStyles = false;
+            this.dgvServices.Location = new System.Drawing.Point(0, 0);
+            this.dgvServices.Name = "dgvServices";
+            this.dgvServices.RowHeadersVisible = false;
+            this.dgvServices.RowHeadersWidth = 51;
+            this.dgvServices.RowTemplate.Height = 24;
+            this.dgvServices.Size = new System.Drawing.Size(967, 186);
+            this.dgvServices.TabIndex = 3;
+            this.dgvServices.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvServices_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSubmit);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.btnCash);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 133);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(967, 53);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Image = ((System.Drawing.Image)(resources.GetObject("btnSubmit.Image")));
+            this.btnSubmit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSubmit.Location = new System.Drawing.Point(853, 3);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(111, 47);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(158, 3);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(697, 50);
+            this.txtSearch.TabIndex = 11;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // btnCash
+            // 
+            this.btnCash.BackColor = System.Drawing.Color.White;
+            this.btnCash.FlatAppearance.BorderSize = 0;
+            this.btnCash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCash.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCash.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCash.Image = ((System.Drawing.Image)(resources.GetObject("btnCash.Image")));
+            this.btnCash.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCash.Location = new System.Drawing.Point(6, 0);
+            this.btnCash.Name = "btnCash";
+            this.btnCash.Size = new System.Drawing.Size(153, 50);
+            this.btnCash.TabIndex = 10;
+            this.btnCash.Text = "Search here";
+            this.btnCash.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCash.UseVisualStyleBackColor = false;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "No";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 62;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "ID";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Visible = false;
+            this.Column2.Width = 55;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Name";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Price";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            // 
+            // Select
+            // 
+            this.Select.HeaderText = "Select";
+            this.Select.MinimumWidth = 6;
+            this.Select.Name = "Select";
+            this.Select.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Select.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Select.Width = 125;
+            // 
+            // CashService
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(967, 186);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvServices);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CashService";
+            this.Text = "CashService";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServices)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvServices;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnCash;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
+    }
+}
---
+++ CashService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Management;
+
+
+namespace Car_Wash_Management
+{
+    public partial class CashService : Form
+    {
+        private SqlConnection Conn;
+        private SqlCommand Cm;
+        SqlDataReader dr;
+        string title = "Car Wash Management";
+        Cash_Form cash;
+        public CashService(Cash_Form cashhform)
+        {
+            InitializeComponent();
+            cash = cashhform;
+            // Use SqlConnectionStringBuilder to create  the connection string
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = "KAMAL\\MSSQLSERVER01", // Replace with your server name or IP address
+                InitialCatalog = "car wash",
+                IntegratedSecurity = true
+            };
+            Conn = new SqlConnection(builder.ConnectionString);
+            loadService();
+        }
+        #region method
+        public void loadService()
+        {
+            try
+            {
+                int i = 0; // To show the number for the service list
+                dgvServices.Rows.Clear();
+                string query = "SELECT id, servicename, price FROM tbService WHERE CONCAT(id, servicename, price) LIKE '%' + @search + '%'";
+                Cm = new SqlCommand(query, Conn);
+                Cm.Parameters.AddWithValue("@search", txtSearch.Text);
+                Conn.Open();
+                dr = Cm.ExecuteReader();
+                while (dr.Read())
+                {
+                    i++;
+                    dgvServices.Rows.Add(i, dr["id"].ToString(), dr["servicename"].ToString(), dr["price"].ToString());
+                }
+                Conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, title);
+            }
+            finally
+            {
+                if (Conn.State == ConnectionState.Open)
+                {
+                    Conn.Close();
+                }
+            }
+        }
+
+        #endregion method
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow dr in dgvServices.Rows)
+            {
+                bool chkbox = Convert.ToBoolean(dr.Cells["Select"].Value);
+                if (chkbox)
+                {
+                    try
+                    {
+                        if (Conn.State == ConnectionState.Open)
+                        {
+                            Conn.Close();
+                        }
+
+                        Cm = new SqlCommand("IF NOT EXISTS (SELECT * FROM tbCash WHERE sid = @sid AND transno = @transno) " +
+                                            "INSERT INTO tbCash (transno, cid, sid, price, date) VALUES (@transno, @cid, @sid, @price, @date)", Conn);
+
+                        Cm.Parameters.AddWithValue("@transno", cash.lblTransno.Text);
+                        Cm.Parameters.AddWithValue("@cid", cash.customerId);
+                        Cm.Parameters.AddWithValue("@sid", dr.Cells[1].Value.ToString());
+                        Cm.Parameters.AddWithValue("@price", Convert.ToDecimal(dr.Cells[3].Value));
+                        Cm.Parameters.AddWithValue("@date", DateTime.Now);
+
+                        Conn.Open();
+                        Cm.ExecuteNonQuery();
+                        Conn.Close();
+
+                        cash.btnCash.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, title);
+                        if (Conn.State == ConnectionState.Open)
+                        {
+                            Conn.Close();
+                        }
+                    }
+                }
+            }
+            this.Dispose();
+            cash.panelCash.Height = 1;
+            cash.loadcash();
+        }
+
+
+
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            loadService();
+        }
+
+        private void dgvServices_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
